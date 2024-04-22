81c727905ebc59ea45af8f1065cced476361ff84
VSouvv <161387076+VSouvv@users.noreply.github.com>
2024-04-21 20:07:46 -0500

    revert back to where we finished like button

--- scripts/index.js
+++ scripts/index.js
@@ -64,31 +64,24 @@ function renderCard(cardData) {
 }
 
 function getCardElement(cardData) {
-  // clone the template element with all its content and store it in a cardElement variable
   const cardElement = cardTemplate.cloneNode(true);
-  // access the card title and image and store them in variables
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
   const likeButton = cardElement.querySelector(".card__like-button");
-  // find delete button
-
-  //add the event listener to the delete button
-  //cardElement.remove(); when b utton is clciked
-
-  //add click listener to the cardImage element
-  //openModal with previewImageModal
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__like-button_active");
   });
 
-  // set the path to the image to the link field of the object
-  // set the image alt text to the name field of the object
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
   cardImageEl.alt = cardData.name;
   cardImageEl.src = cardData.link;
-  // set the card title to the name field of the object, too
   cardTitleEl.textContent = cardData.name;
-  // return the ready HTML element with the filled-in data
+
   return cardElement;
 }
 
