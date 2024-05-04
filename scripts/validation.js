e7881534ac0840f427dfaf7dcb42647419e20af4
VSouvv <161387076+VSouvv@users.noreply.github.com>
2024-05-04 13:37:58 -0500

    validation js added

---
+++ scripts/validation.js
@@ -0,0 +1,18 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(".modal__form")];
+  console.log(formEls);
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
